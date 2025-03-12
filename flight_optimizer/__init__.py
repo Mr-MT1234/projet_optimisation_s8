23dbe75b6f80e950d5896e9286553297d65b057d
Mohamed Tribak <tribakmoha31@gmail.com>
2025-03-12 10:12:06 +0100

    Finalized FlowSolver

--- flight_optimizer/__init__.py
+++ flight_optimizer/__init__.py
@@ -5,4 +5,3 @@ from .reporter import Reporter
 from .solvers.solver import Solver
 from .solvers.interval_solver import IntervalSolver
 from .solvers.flow_solver import FlowSolver
-from .solvers.flow_solver2 import FlowSolver2
