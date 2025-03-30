0b4ccde2903f8fe5981a7a2eb766a430391726d4
Mohamed Tribak <tribakmoha31@gmail.com>
2025-03-30 22:06:54 +0200

    Deleted some unnecessairy files

--- validate_reduced_maintenance.py
+++ validate_reduced_maintenance.py
@@ -2,16 +2,17 @@ from flight_optimizer import *
 import pickle
 import os
 
-SOLUTION_DIR = "./benchmarks/reduced_flow_maintenance"
+SOLUTION_DIR = "./benchmarks/interval"
 
 for solution_name in os.listdir(SOLUTION_DIR):
     print(f"testing solution {solution_name}.", end='')
-    solution_path = os.path.join(SOLUTION_DIR, solution_name,'solution.pickle')
-
-    solution = pickle.load(open(solution_path, 'rb'))
-
-    valid, reason = solution.is_valide()
-    if not valid:
-        print(f' found problem: {reason}')
-    else:
-        print(f" Solution is correct")
+    try:
+        solution_path = os.path.join(SOLUTION_DIR, solution_name,'solution.pickle')
+        solution = pickle.load(open(solution_path, 'rb'))
+        valid, reason = solution.is_valide()
+        if not valid:
+            print(f' found problem: {reason}')
+        else:
+            print(f" Solution is correct")
+    except FileNotFoundError:
+        print("  ")
